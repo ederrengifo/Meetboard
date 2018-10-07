98787ed09884a128d1a80c737b0fc08a11bd6c00
Eder Rengifo <rengifo.eder@gmail.com>
2018-10-07 00:33:23 -0500

    small updates in oauth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,29 +2,32 @@ require 'google/api_client/client_secrets.rb'
 require 'google/apis/calendar_v3'
 
 class HomeController < ApplicationController
+
   def show
+
   end
 
   private
-
-  def google_secret
-    Google::APIClient::ClientSecrets.new(
-      { "web" =>
-        { "access_token" => @user.google_token,
-          "refresh_token" => @user.google_refresh_token,
-          "client_id" => Rails.application.secrets.google_client_id,
-          "client_secret" => Rails.application.secrets.google_client_secret,
+  
+    def google_secret
+      Google::APIClient::ClientSecrets.new(
+        { "web" =>
+          { "access_token" => @user.google_token,
+            "refresh_token" => @user.google_refresh_token,
+            "client_id" => Rails.application.secrets.google_client_id,
+            "client_secret" => Rails.application.secrets.google_client_secret,
+          }
         }
-      }
-    )
-  end
+      )
+    end
 
-  def get_calendars
-    service = Google::Apis::CalendarV3::CalendarService.new
-    service.authorization = google_secret.to_authorization
-    service.authorization.refresh!
+    def get_calendars
+      service = Google::Apis::CalendarV3::CalendarService.new
+      service.authorization = google_secret.to_authorization
+      service.authorization.refresh!
 
-    @calendar = service.list_calendar_lists
-  end
+      calendar_list = service.list_calendar_lists.items
+
+    end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
+require 'google/api_client/client_secrets.rb'
+require 'google/apis/calendar_v3'
+
 class SessionsController < ApplicationController
   def create
     access_token = request.env["omniauth.auth"]
     user = User.from_omniauth(access_token)
-    # log_in(user)
     session[:user_id] = user.id
-
     user.google_token = access_token.credentials.token
     refresh_token = access_token.credentials.refresh_token
     user.google_refresh_token = refresh_token if refresh_token.present?
