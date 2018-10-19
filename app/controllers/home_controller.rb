98112a57ce501524b88b69bb05f2396489fcf68a
Eder Rengifo <rengifo.eder@gmail.com>
2018-10-19 02:29:39 -0500

    adding style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,8 +20,6 @@ class HomeController < ApplicationController
           "refresh_token" => user.google_refresh_token,
           "client_id" => Rails.application.secrets.google_client_id,
           "client_secret" => Rails.application.secrets.google_client_secret,
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://accounts.google.com/o/oauth2/token",
         }
       }
     )
@@ -29,14 +27,15 @@ class HomeController < ApplicationController
 
   def get_calendar(user)
     service = Google::Apis::CalendarV3::CalendarService.new
-    service.client_options.application_name = "Meetboard"
     service.authorization = google_secret(user).to_authorization
-
+    # service.authorization.refresh!
+    
     event_list = service.list_events('primary',
                                       single_events: true, 
                                       order_by: 'startTime', 
-                                      max_results: 10,
-                                      time_min: Time.now.iso8601).items
+                                      max_results: 20,
+                                      time_min: Time.now.iso8601,).items
+
   end
 
 end
